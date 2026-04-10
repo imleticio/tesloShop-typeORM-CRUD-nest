7dfdae68c2e1002f05362067c5b5e1e0624d8605
imleticio <leonel_martinez00@hotmail.com>
2026-04-10 16:41:37 -0300

    acciones por roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards, Request, Headers, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto , LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
@@ -6,6 +6,10 @@ import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities';
 import { GetRawHeader } from './decorators/get-rawHeader.decorator';
 import { IncomingHttpHeaders } from 'http';
+import { UserRoleGuard } from './guards/user-role.guard';
+import { RoleProtected } from './decorators/role-protected.decorator';
+import { validRoles } from './interfaces';
+import { Auth } from './decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -29,19 +33,56 @@ export class AuthController {
     @GetUser() user:User,
     @GetUser('email') userEmail:string,
     @GetRawHeader() rawHeader:string[],
-    // @Headers() header:IncomingHttpHeaders
+    @Headers() headers:IncomingHttpHeaders
   ){
-    // console.log({user:request.user})
-    // console.log(request)
+    
     return{
       ok:true,
       message:'Hola mundo',
       user,
       userEmail,
       rawHeader,
-      // header
+      headers
     }
+
+
+
+    
   }
+
+  @Get('private2')
+  @RoleProtected(validRoles.superUser, validRoles.admin)
+  // @SetMetadata('roles',['admin','super-user'])
+  @UseGuards(AuthGuard('jwt'),UserRoleGuard)
+  privateRoute2(
+    @GetUser() user : User
+  ){
+
+    return {
+    ok:true,
+    user
+
+  }
+
+
+  }
+   @Get('private3')
+   @Auth(validRoles.admin)
+
+  privateRoute3(
+    @GetUser() user : User
+  ){
+
+    return {
+    ok:true,
+    user
+
+  }
+
+
+  }
+
+  
 
 
 }
---
+++ src/auth/guards/user-role.guard.ts
@@ -0,0 +1,41 @@
+import { BadGatewayException, CanActivate, ExecutionContext, ForbiddenException, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { User } from '../entities';
+import { validateEach } from '@nestjs/common/utils/validate-each.util';
+import { META_ROLES } from '../decorators/role-protected.decorator';
+import { ReturnDocument } from 'typeorm';
+
+@Injectable()
+export class UserRoleGuard implements CanActivate {
+
+  constructor(
+    private readonly reflector:Reflector
+  ){}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const validRoles: string[] = this.reflector.get(META_ROLES, context.getHandler())
+
+    if (!validRoles) return true;
+    if(validRoles.length ===0) return true;
+    
+    const req = context.switchToHttp().getRequest();
+    const user =req.user as User
+    if(!user){
+      throw new BadGatewayException('user not found');
+    }
+    console.log({userRoles:user.roles})
+
+    for (const role of user.roles) {
+      if(validRoles.includes(role)){
+        return true;
+      }
+    }
+
+    throw new ForbiddenException(
+      `User ${user.fullName} need a valid role [${validRoles}]`
+    )
+  }
+}
